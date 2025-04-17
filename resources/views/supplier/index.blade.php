2f62452019fa5b7cc3090078cc8eedb0346d26e3
agungfradiansyah12 <agungfradiansyah01@gmail.com>
2025-04-18 00:58:05 +0700

    TUGAS JB O8 Praktikum02

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -5,9 +5,10 @@
     <div class="card-header">
         <h3 class="card-title">{{ $page->title }}</h3>
         <div class="card-tools">
-            <button onclick="modalAction('{{ url('/supplier/import') }}')" class="btn btn-sm btn-info mt-1">Import Kategori</button>
-            <a class="btn btn-sm btn-primary mt-1" href="{{ url('supplier/create') }}">Tambah</a>
-            <button onclick="modalAction('{{ url('/supplier/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+            <button onclick="modalAction('{{ url('/supplier/import') }}')" class="btn btn-info">Import Kategori</button>
+            {{-- <a class="btn btn-sm btn-primary mt-1" href="{{ url('supplier/create') }}">Tambah</a> --}}
+            <a href="{{ url('/supplier/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i> Export Supplier</a> 
+            <button onclick="modalAction('{{ url('/supplier/create_ajax') }}')" class="btn btn-success">Tambah Ajax</button>
         </div>
     </div>
     <div class="card-body">
