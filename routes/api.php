c865c9030daae312953855ac2c690b5ac46aaa02
agungfradiansyah12 <agungfradiansyah01@gmail.com>
2025-05-05 00:33:14 +0700

    tugas jobsheet10

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\RegisterController;
-use App\Http\Controllers\Api\LoginController;
-use App\Http\Controllers\Api\LogoutController;
-use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,18 +13,55 @@ use App\Http\Controllers\Api\LevelController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::post('/register', RegisterController::class)->name('register');
-Route::post('/login', LoginController::class)->name('login');
-
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
-Route::post('/logout', LogoutController::class)->name('logout');
-
+use App\Http\Controllers\Api\LevelController;
 Route::get('levels', [LevelController::class, 'index']);
 Route::post('levels', [LevelController::class, 'store']);
 Route::get('levels/{level}', [LevelController::class, 'show']);
 Route::put('levels/{level}', [LevelController::class, 'update']);
 Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+use App\Http\Controllers\Api\UserController;
+Route::get('users', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::get('users/{user}', [UserController::class, 'show']);
+Route::put('users/{user}', [UserController::class, 'update']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
+
+use App\Http\Controllers\Api\KategoriController;
+
+Route::get('kategori', [KategoriController::class, 'index']);
+Route::post('kategori', [KategoriController::class, 'store']);
+Route::get('kategori/{kategori}', [KategoriController::class, 'show']);
+Route::put('kategori/{kategori}', [KategoriController::class, 'update']);
+Route::delete('kategori/{kategori}', [KategoriController::class, 'destroy']);
+
+use App\Http\Controllers\Api\BarangController;
+
+Route::get('barang', [BarangController::class, 'index']);
+Route::post('barang', [BarangController::class, 'store']);
+Route::get('barang/{barang}', [BarangController::class, 'show']);
+Route::put('barang/{barang}', [BarangController::class, 'update']);
+Route::delete('barang/{barang}', [BarangController::class, 'destroy']);
+
+use App\Http\Controllers\Api\SupplierController;
+
+Route::get('supplier', [SupplierController::class, 'index']);
+Route::post('supplier', [SupplierController::class, 'store']);
+Route::get('supplier/{supplier}', [SupplierController::class, 'show']);
+Route::put('supplier/{supplier}', [SupplierController::class, 'update']);
+Route::delete('supplier/{supplier}', [SupplierController::class, 'destroy']);
+
+
+
+
+
+
+
+
